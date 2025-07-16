bc928c4e5a4a5b3dfce25e31d9cba6d1fbad9903
Ali Nowia <142123646+AliNowia@users.noreply.github.com>
2025-07-16 14:37:27 +0300

    Update Merge_the_Tools!.py

--- Merge_the_Tools!.py
+++ Merge_the_Tools!.py
@@ -1,4 +1,5 @@
-# TEST_CASE 15 IS WRONG ANSWER. I don't remember why.
+# RUNTIME ERROR AT TEST CASES 14,15
+# NEEDS OPTIMIZATION
 
 def is_in(c, s):
     for l in s:
@@ -10,22 +11,22 @@ def merge_the_tools(string, k):
     n = len(string)
     t = []
     st, e = 0, k
-
-    for i in range(int(n/k)):
-        if i > n-(n/k):
-            break
-        t.append(string[st:e])
-        st=e
-        e = e + k
-
-    for k in range(len(t)):
-        temp = ""
-        for i in range(0, len(t[k])):
-            if is_in(t[k][i], temp):
-                continue
-            temp = temp + t[k][i]
-        t[k] = temp
-        temp = ""
     
+    if k > 1:
+        for i in range(int(n/k)):
+            t.append(string[st:e])
+            st=e
+            e = e + k
+            
+        for u in range(len(t)):
+            temp = ""
+            for i in range(0, len(t[u])):
+                if is_in(t[u][i], temp):
+                    continue
+                temp = temp + t[u][i]
+            t[u] = temp
+    else:
+        t = [c for c in s]
+        
     for i in range(len(t)):
         print(t[i])
