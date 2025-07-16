23c1534a7607c3e233298f42818735a89c691a01
Ali Nowia <142123646+AliNowia@users.noreply.github.com>
2025-07-16 14:32:54 +0300

    Create Merge_the_Tools!.py

---
+++ Merge_the_Tools!.py
@@ -0,0 +1,29 @@
+def is_in(c, s):
+    for l in s:
+        if l == c:
+            return True
+    return False
+    
+def merge_the_tools(string, k):
+    n = len(string)
+    t = []
+    st, e = 0, k
+
+    for i in range(int(n/k)):
+        if i > n-(n/k):
+            break
+        t.append(string[st:e])
+        st=e
+        e = e + k
+
+    for k in range(len(t)):
+        temp = ""
+        for i in range(0, len(t[k])):
+            if is_in(t[k][i], temp):
+                continue
+            temp = temp + t[k][i]
+        t[k] = temp
+        temp = ""
+    
+    for i in range(len(t)):
+        print(t[i])
